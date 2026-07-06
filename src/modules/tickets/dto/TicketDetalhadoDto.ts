d53bd6e74fb3182b93e82066044e0bd19b63e5da
pedroadm22 <p.plmr45@gmail.com>
2026-07-06 03:06:29 -0300

    fix: retirados alguns arquivos que estavam resultando em erro de compilacao e prejudicando o build da aplicacao

--- src/modules/tickets/dto/TicketDetalhadoDto.ts
+++
@@ -1,7 +0,0 @@
-import { TicketSelect, UserSelect } from "@/infrastructure/schema/ticket";
-
-export interface TicketDetalhadoDto extends TicketSelect {
-  // Injetamos os dados completos do usuário em vez de apenas os IDs soltos
-  cliente: Omit<UserSelect, "dataCriacao">; 
-  tecnico: Omit<UserSelect, "dataCriacao"> | null; // Pode ser nulo se não houver triagem
-}
